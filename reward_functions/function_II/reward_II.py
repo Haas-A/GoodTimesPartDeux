70b9d5789e7bdb1a3ce1d681d9f807c7ed83aaab
Alex <77426531+Haas-A@users.noreply.github.com>
2023-07-06 15:37:16 -0400

    finished second reward function

--- reward_functions/function_II/reward_II.py
+++ reward_functions/function_II/reward_II.py
@@ -8,16 +8,19 @@ def reward_function(params):
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
     heading = params['heading']
-    off_track = params['is_offtrack']
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
     speed = params['speed']
 
+    #Threshold the car should aim for the target waypoint
+    direction_threshold = 10
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15
+    future_waypoint = 4
     num_waypoints = len(waypoints)
-    target_waypoint_index = closest_waypoints[0] + 5
+    target_waypoint_index = closest_waypoints[0] + future_waypoint
     target_waypoint_index = target_waypoint_index % len(waypoints)
     target_waypoint = waypoints[target_waypoint_index]
     prev_waypoint = waypoints[closest_waypoints[0]]
-    direction_threshold = 10
     reward = 1.0
 
     # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
@@ -30,18 +33,17 @@ def reward_function(params):
     if direction_diff > 180:
         direction_diff = 360 - direction_diff
 
-    #TODO write rewards for the direction the car is heading
-    # I think we will want a positive reinforcement if the car is heading
-    # in the direction of the future waypoint. It will have negative reinforcement
-    # if the car is heading too far away from the direction of the future waypoint.
-    # It will have a large reward for going fast if the future waypoint is less than a X degree
-    # turn (or something of the like) from the current position (or the current waypoint.)
-
-
-    #----We can use the steering penalty threshold below if we desire, unsure if we should in this iteration ----
-
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 22
+    #reward for going fast when heading towards future waypoint
+    if direction_diff < direction_threshold and speed > 3.75:
+        reward *= 2
+    elif direction_diff < direction_threshold * 2 and speed > 2.75:
+        reward *= 1.3
+    elif direction_diff > 70 and speed > 2.75:
+        reward *= 0.2
+    
+    #reward for going slow at a turn
+    if track_direction > 75 and speed < 3:
+        reward *= 1.5
 
     # Penalize reward if the car is steering too much
     if abs_steering > ABS_STEERING_THRESHOLD:
